898e2330526040475826bbec5fa60580ac137616
YaronMiro <yaron@gizra.com>
2016-06-14 20:09:41 +0300

    Update 2016-06-14T17:09:39.045Z

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -31,28 +31,28 @@ function testBrowser() {
  * Adding the capability to hide/show dropdown menu.
  *
  */
-function navigationDropdown() {
+function collapse() {
 // Get the target element ID.
-  $( '[data-dropdown-target]').click(function(event) {
+  $( '[data-collapse-target]').click(function(event) {
 
     // The "trigger" element that was selected.
     var triggerElement = event.target;
 
-    // The target dropdown to "activate".
-    var targetId = $(triggerElement).data('dropdownTarget');
+    // The target collapse to "activate".
+    var targetId = $(triggerElement).data('collapseTarget');
     var $target = $(targetId);
 
-    // In case we want to deactivate an "active" dropdown.
+    // In case we want to deactivate an "active" collapse.
     if ($target.is(":visible")) {
       $(triggerElement).removeClass('nav__list__item--active');
       $target.removeClass('header__container--visible');
     }
     else {
-      // Hide any other "active" dropdown.
+      // Hide any other "active" collapse.
       $('.header__container--visible').removeClass('header__container--visible');
       $('.nav__list__item--active').removeClass('nav__list__item--active');
 
-      // "activate" the target dropdown.
+      // "activate" the target collapse.
       $($target).addClass('header__container--visible');
       $(triggerElement).addClass('nav__list__item--active');
 
@@ -61,7 +61,7 @@ function navigationDropdown() {
 }
 
 testBrowser();
-navigationDropdown();
+collapse();
 
 $(function () {
 
